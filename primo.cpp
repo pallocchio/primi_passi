489dc056e7427a82df6147cf7797a96a024a5e3e
pallocchio <73364565+pallocchio@users.noreply.github.com>
2020-10-23 21:40:13 +0100

    Update primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -1,9 +1,12 @@
-
 #include <fmt/format.h>
 
 /**
  * int è il risultato della funzione che è chiamata main
  * un intero è un numero senza virgola
+ * 
+ * una funzione è un poco come una ricetta di cucina 
+ * ci sono alcuni ingredienti ( variabili, constanti e parametri)
+ * ed alcune operazioni da fare nel corretto ordine 
  */
 int main() {
 	//Definisco una costante, chiamata inizio, di tipo numero con la virgola e di valore -100..
@@ -12,15 +15,15 @@ int main() {
 	//intervallo di simulazione
 	const float passo = 33.3;
 
-	//stampa le lettere 'c' ed 'f' al centro di un bloco di 7 carateri 
-	fmt::print("{:^7} {:^7} \n","°C","°F");
-	
-	//Per un valore che parte da inizio, fino ad arrivare a fine... a passi di passo eseguire quello che è fra parentesi
+	//stampa le 2 lettere 'c' ed 'f' al centro di un blocco di 7 carateri
+	fmt::print("{:^7} {:^7} \n", "°C", "°F");
+
+	//fare che la variabile celsius parta dal valore di inizio, arrivi al valore di fine, incrementandosi ad ogni iterazione del valore passo
 	for (float celsius = inizio; celsius < fine; celsius += passo) {
 		//L'equazione di conversione, la copia incolliamo da wikipedia, poteva esser anche la conversione da florz a bnoff
-		float fahrenheit = celsius * (9.0 / 5.0) + 32;
+		const float fahrenheit = celsius * (9.0 / 5.0) + 32;
 		//Della libreria usa la funzione print
-		//La sintassi {} definisce dove mettere un valore (che verrà indicato in seguito) e si chiama segnaposto 
-		fmt::print("{:7.1f} {:7.1f} \n",celsius,fahrenheit);
+		//La sintassi {} definisce dove mettere un valore (che verrà indicato in seguito) e si chiama segnaposto
+		fmt::print("{:7.1f} {:7.1f} \n", celsius, fahrenheit);
 	}
 }
